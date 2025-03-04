f3da3605c776a3d55f0e875ef58a45eebae39013
Özge Odabaş <111779365+ozgeodbs@users.noreply.github.com>
2025-03-04 22:10:09 +0300

    main page, agenda and speakers created

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("about.html", title="Ana Sayfa")
+    return render_template("index.html", title="Ana Sayfa")
 
-@app.route('/overview')
-def about():
-    return render_template("base.html", title="Program")
+@app.route('/agenda')
+def agenda():
+    return render_template("agenda.html")
 
-@app.route('/iletisim')
-def contact():
-    return render_template("contact.html", title="İletişim")
+@app.route('/speakers')
+def speakers():
+    return render_template("speakers.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
